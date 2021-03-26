de5a4cc15a4bbd930b76695e87c65a2c55ecca7e
Andres Diaz <ivanandresdilox@gmail.com>
2021-03-26 14:19:33 -0500

    primera clase de React Hooks: useState, useEffect

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Carousel from '../components/Carousel';
@@ -7,6 +7,13 @@ import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 
 const App = () => {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:3000/initalState')
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
+  console.log(videos);
   return (
     <div className='App'>
       <Header />
